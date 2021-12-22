72a335ba2e2a6e3981a5696a148ccb08886962f6
Agnes Diza Fahira <agnesdizaf@gmail.com>
2021-12-22 10:07:55 +0700

    Add MyContext.cs
    Mengisi class MyContext.cs, di inheritence dengan DbContext dari Microsoft.EntitiyFrameworkCore, dan diisi public classnya dengan DbSet<Employee> untuk menghubungkan dengan class Employee

--- Context/MyContext.cs
+++ Context/MyContext.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MCC61_API_Project.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MCC61_API_Project.Context
 {
-    public class MyContext
+    public class MyContext : DbContext
     {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; } //menghubungi table ke database di sql server
     }
 }
