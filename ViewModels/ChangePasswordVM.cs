00435a12fc7b2a6aeb8fb00ca88ed2df21cfb875
Agnes Diza Fahira <agnesdizaf@gmail.com>
2021-12-27 21:45:51 +0700

    Add ChangePasswordVM
    Menambahkan ViewModel untuk change password

--- ViewModels/ChangePasswordVM.cs
+++ ViewModels/ChangePasswordVM.cs
@@ -7,5 +7,8 @@ namespace MCC61_API_Project.ViewModels
 {
     public class ChangePasswordVM
     {
+        public string Email { get; set; }
+        public int OTP { get; set; }
+        public string NewPassword { get; set; }
     }
 }
