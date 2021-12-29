d1ad059d107c7ae8b9e507850fae76920bb8eb38
Agnes Diza Fahira <agnesdizaf@gmail.com>
2021-12-29 17:16:08 +0700

    Add Jwt Token at Method Login

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using MCC61_API_Project.Context;
 using MCC61_API_Project.Models;
 using MCC61_API_Project.Repository.Data;
 using MCC61_API_Project.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MCC61_API_Project.Controllers
 {
@@ -18,10 +24,12 @@ namespace MCC61_API_Project.Controllers
     {
         private readonly MyContext context;
         private readonly AccountRepository accountRepository;
-        public AccountsController(AccountRepository accountRepository, MyContext myContext) : base(accountRepository)
+        public IConfiguration _configuration;
+        public AccountsController(AccountRepository accountRepository, MyContext myContext, IConfiguration configuration) : base(accountRepository)
         {
             this.accountRepository = accountRepository;
             this.context = myContext;
+            this._configuration = configuration;
         }
 
 
@@ -36,7 +44,30 @@ namespace MCC61_API_Project.Controllers
                 switch (login)
                 {
                     case 1:
-                        return Ok(new { status = HttpStatusCode.OK, login = login, message = "Login Success" });
+                        var getRoles = accountRepository.GetRoles(loginVM.Email);
+
+                        var claims = new List<Claim>
+                        {
+                            new Claim("email", loginVM.Email)
+
+                        };
+                        foreach (var roleData in getRoles)
+                        {
+                            claims.Add(new Claim("roles", roleData.ToString()));
+                        }
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                        var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                                _configuration["Jwt:Issuer"],
+                                _configuration["Jwt:Audience"],
+                                claims,
+                                expires: DateTime.UtcNow.AddMinutes(10),
+                                signingCredentials: signIn
+                            );
+                        var idToken = new JwtSecurityTokenHandler().WriteToken(token);
+                        claims.Add(new Claim("TokenSecurity", idToken.ToString()));
+                        return Ok(new { status = HttpStatusCode.OK, idtoken = idToken, message = "Login Success" });
                     case 2:
                         return Ok(new { status = HttpStatusCode.BadRequest, login = login, message = "Wrong Password" });
                     case 3:
@@ -63,7 +94,7 @@ namespace MCC61_API_Project.Controllers
                 {
                     case 1:
                         return Ok(new { status = HttpStatusCode.OK, result = forgotPassword, message = "Check your email for the OTP" });
-                    case 2: 
+                    case 2:
                         return Ok(new { status = HttpStatusCode.NotFound, result = forgotPassword, message = "Your email was not found in the database" });
                     default:
                         return Ok(new { status = HttpStatusCode.BadRequest, result = forgotPassword, message = "Failed to send OTP to your email" });
@@ -104,5 +135,6 @@ namespace MCC61_API_Project.Controllers
                 throw;
             }
         }
+
     }
 }
